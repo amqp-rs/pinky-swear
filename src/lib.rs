735ed64797537ea81a8fd2ad9cef4149cba001c8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-09-17 21:54:23 +0200

    v4.3.3
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 #![doc(test(attr(deny(rust_2018_idioms, warnings))))]
 #![doc(test(attr(allow(unused_extern_crates))))]
-#![doc(html_root_url = "https://docs.rs/pinky-swear/4.3.2/")]
+#![doc(html_root_url = "https://docs.rs/pinky-swear/4.3.3/")]
 
 doc_comment::doctest!("../README.md");
 
